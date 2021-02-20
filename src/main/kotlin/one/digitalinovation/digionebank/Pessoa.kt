6d3f617a672c8f503a3137c3c97afc1989c36c37
Eduardo Yoshizato <eduyoshi@gmail.com>
2021-02-20 08:45:19 -0300

    update to talk about class members

--- src/main/kotlin/one/digitalinovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinovation/digionebank/Pessoa.kt
@@ -5,14 +5,17 @@ class Pessoa {
 
     var cpf: String = "123.123.123-11"
     private set
+
+    constructor ()
+
+    fun pessoaInfo() = "Nome:$nome e cpf:$cpf"
+
 }
 
 fun main (){
     val eduardo = Pessoa()
 
     println(eduardo)
-    println(eduardo.nome)
-    println(eduardo.cpf)
-
+    println(eduardo.pessoaInfo())
 
 }
