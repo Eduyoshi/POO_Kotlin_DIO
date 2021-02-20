dbecf75bd80e4c8fe7f4d27f24adb6254e64ecd6
Eduardo Yoshizato <eduyoshi@gmail.com>
2021-02-20 08:33:49 -0300

    update to apply the private modifier to set function of cpf property

--- src/main/kotlin/one/digitalinovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinovation/digionebank/Pessoa.kt
@@ -2,13 +2,17 @@ package one.digitalinovation.digionebank
 
 class Pessoa {
     var nome: String = "Eduardo"
-    var cpf: String = "123.123.123-11"
 
+    var cpf: String = "123.123.123-11"
+    private set
 }
 
 fun main (){
     val eduardo = Pessoa()
 
+    println(eduardo)
     println(eduardo.nome)
     println(eduardo.cpf)
+
+
 }
