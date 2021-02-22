c33a7f6aeb03f99829a6330b7d25297d6caf725f
Eduardo Yoshizato <eduyoshi@gmail.com>
2021-02-22 16:51:06 -0300

    Update to make person and employed abstract

---
+++ src/main/kotlin/one/digitalinovation/digionebank/Funcionario.kt
@@ -0,0 +1,12 @@
+package one.digitalinovation.digionebank
+
+import java.math.BigDecimal
+
+
+abstract class Funcionario (
+        nome: String,
+        cpf: String,
+        val salario: BigDecimal
+        ):Pessoa(nome, cpf) {
+                abstract fun calculaAuxilio();
+}
--- src/main/kotlin/one/digitalinovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinovation/digionebank/Pessoa.kt
@@ -1,21 +1,6 @@
 package one.digitalinovation.digionebank
 
-class Pessoa {
-    var nome: String = "Eduardo"
-
-    var cpf: String = "123.123.123-11"
-    private set
-
-    constructor ()
-
-    fun pessoaInfo() = "Nome:$nome e cpf:$cpf"
-
-}
-
-fun main (){
-    val eduardo = Pessoa()
-
-    println(eduardo)
-    println(eduardo.pessoaInfo())
-
-}
+abstract class Pessoa(
+        val nome: String,
+        val cpf: String
+)
